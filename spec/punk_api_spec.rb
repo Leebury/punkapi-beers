c581173c631c36441c117783be9d77dbc80bc076
Tech-A21 <Tech-A21@Tech-A21s-MacBook-Air.local>
2017-08-01 15:33:58 +0100

    18 completed tests

--- spec/punk_api_spec.rb
+++ spec/punk_api_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
+require_relative '../punk_api.rb'
 
 # THIS WILL BE FOR OUR CLASS/MODULE
-describe "PUNK API OBJECT/CLASS/MODULE" do
+describe PunkApi do
 	
 	before(:all) do
 		# THIS WILL BE AN INSTANCE OF OUR CLASS/MODULE
-		@beer = get("https://api.punkapi.com/v2/beers")
-		puts @beer
+		@all_beers = JSON.parse get("https://api.punkapi.com/v2/beers")
+		
 	end
 
 	# THE TESTS WILL GO HERE
@@ -21,166 +22,264 @@ describe "PUNK API OBJECT/CLASS/MODULE" do
 	# end
 
 	it "should return an array, containing individual hashes for each beer" do
+		expect_json_types(:array)
+		@all_beers.each do |hash|
+		 	expect(hash.class).to be(Hash)
+		 end
 	end
 
-	it "should have a key called id, of datatype integer" do
+	it "should have a key called id, of datatype integer" do		
+		 @all_beers.each do |hash|
+		 	expect(hash).to include("id")
+		 	expect(hash["id"].class).to be(Integer)
+		 end
 	end
 
 	it "should have a key called name, of datatype string" do
+		@all_beers.each do |hash|
+		 	expect(hash).to include("name")
+		 	expect(hash["name"].class).to be(String)
+		 end
 	end
 
 	it "should have a key called tagline, of datatype string" do
+		@all_beers.each do |hash|
+		 	expect(hash).to include("tagline")
+		 	expect(hash["tagline"].class).to be(String)
+		 end
 	end
 
 	it "should have a key called first_brewed, of datatype string" do
+		@all_beers.each do |hash|
+		 	expect(hash).to include("first_brewed")
+		 	expect(hash["first_brewed"].class).to be(String)
+		 end
 	end
 
 	it "should have a key called description, of datatype string" do
+		@all_beers.each do |hash|
+		 	expect(hash).to include("description")
+		 	expect(hash["description"].class).to be(String)
+		 end
 	end
 
 	it "should have a key called image_url, of datatype string" do
+		@all_beers.each do |hash|
+		 	expect(hash).to include("image_url")
+		 	expect(hash["image_url"].class).to be(String)
+		 end
 	end
 
-	it "should have a key called abv, of datatype float" do
+	it "should have a key called abv, of datatype float or integer" do
+			@all_beers.each do |hash|
+		 	expect(hash).to include("abv")
+		 	expect(hash["abv"].class).to eq(Integer).or eq(Float)
+		 end
 	end
 
-	it "should have a key called ibu, of datatype float OR null" do
+	it "should have a key called ibu, of datatype float, integer OR nil" do
+			@all_beers.each do |hash|
+		 	expect(hash).to include("ibu")
+		 	expect(hash["ibu"].class).to eq(Float).or eq(Integer).or eq(NilClass)
+		 end
 	end
 
-	it "should have a key called target_fg, of datatype float OR null" do
+	
+	it "should have a key called target_fg, of datatype float, integer OR nil" do
+			@all_beers.each do |hash|
+		 	expect(hash).to include("target_fg")
+		 	expect(hash["target_fg"].class).to eq(Float).or eq(Integer).or eq(NilClass)
+		 end
 	end
 
-	it "should have a key called target_og, of datatype float OR null" do
+	it "should have a key called target_og, of datatype float OR nil" do
+			@all_beers.each do |hash|
+		 	expect(hash).to include("target_og")
+		 	expect(hash["target_og"].class).to eq(Float).or eq(Integer).or eq(NilClass)
+		 end
 	end
 
-	it "should have a key called ebc, of datatype float OR null" do
+	it "should have a key called ebc, of datatype float OR nil" do
+			@all_beers.each do |hash|
+		 	expect(hash).to include("ebc")
+		 	expect(hash["ebc"].class).to eq(Integer).or eq(NilClass)
+		 end
 	end
 
-	it "should have a key called srm, of datatype float OR null" do
+	it "should have a key called srm, of datatype float, integer OR nil" do
+			@all_beers.each do |hash|
+		 	expect(hash).to include("srm")
+		 	expect(hash["srm"].class).to eq(Float).or eq(Integer).or eq(NilClass)
+		 end
 	end
 
-	it "should have a key called ph, of datatype float OR null" do
+	it "should have a key called ph, of datatype float, integer OR nil" do
+			@all_beers.each do |hash|
+		 	expect(hash).to include("ph")
+		 	expect(hash["ph"].class).to eq(Float).or eq(Integer).or eq(NilClass)
+		 end
 	end
 
-	it "should have a key called attenuation_level, of datatype float OR null" do
+	it "should have a key called attenuation_level, of datatype float, integer OR nil" do
+			@all_beers.each do |hash|
+		 	expect(hash).to include("attenuation_level")
+		 	expect(hash["attenuation_level"].class).to eq(Float).or eq(Integer).or eq(NilClass)
+		 end
 	end
 
 	it "should have a hash called volume" do
+		@all_beers.each do |hash_of_beer|
+			hash_of_beer.each do |k,v|
+				if k == "volume"
+					expect(v).to be_kind_of Hash
+				end
+			end
+		end
 	end
 
 	it "should have a key inside the volume hash called value, of datatype integer" do
+		@all_beers.each do |hash_of_beer|
+			hash_of_beer.each do |key,value|
+				if key == "volume"
+					value.each do|v, data_type|
+						if v == "value"
+							expect(data_type.class).to be Integer
+						end
+					end
+				end
+			end
+		end
 	end
 
 	it "should have a key inside the volume hash called unit, of datatype string" do
+		@all_beers.each do |hash_of_beer|
+			hash_of_beer.each do |key,value|
+				if key == "volume"
+					value.each do|v, data_type|
+						if v == "unit"
+							expect(data_type.class).to be String
+						end
+					end
+				end
+			end
+		end
 	end
 
-	it "should have a hash called boil_volume" do
-	end
+	# it "should have a hash called boil_volume" do
+	# 	@all_beers.each do |hash_of_beer|
+	# 		hash_of_beer.each do |key,value|
+	# 			if key == "boil_volume"
+	# 				expect(value).to be Hash
+	# 			end
+	# 		end
+	# 	end
+	# end
 
-	it "should have a key inside the boil_volume hash called value, of datatype integer" do
-	end
+	# it "should have a key inside the boil_volume hash called value, of datatype integer" do
+	# end
 
-	it "should have a key inside the boil_volume hash called unit, of datatype string" do
-	end
+	# it "should have a key inside the boil_volume hash called unit, of datatype string" do
+	# end
 
-	it "should have a hash called method" do
-	end
+	# it "should have a hash called method" do
+	# end
 
-	it "should have an array inside the method hash called mash_temp" do
-	end
+	# it "should have an array inside the method hash called mash_temp" do
+	# end
 
-	it "should have AT LEAST ONE hash inside the mash_temp array" do
-	end
+	# it "should have AT LEAST ONE hash inside the mash_temp array" do
+	# end
 
-	it "should have a hash inside the hash within the mash_temp array called temp" do
-	end
+	# it "should have a hash inside the hash within the mash_temp array called temp" do
+	# end
 
-	it "should have a key inside the temp hash called value, of datatype integer/FLOAT??? OR null" do
-	end
+	# it "should have a key inside the temp hash called value, of datatype integer/FLOAT??? OR nil" do
+	# end
 
-	# SECOND HALF
+	# # SECOND HALF
 	
-	it "should have a key inside the temp hash called unit, of datatype string" do
-	end
+	# it "should have a key inside the temp hash called unit, of datatype string" do
+	# end
 
-	it "should have a key inside the hash within the mash_temp array called duration, of datatype integer OR null" do
-	end
+	# it "should have a key inside the hash within the mash_temp array called duration, of datatype integer OR nil" do
+	# end
 
-	it "should have a hash inside the method hash called fermentation" do
-	end
+	# it "should have a hash inside the method hash called fermentation" do
+	# end
 
-	it "should have a hash inside the fermentation hash called temp" do
-	end
+	# it "should have a hash inside the fermentation hash called temp" do
+	# end
 
-	it "should have a key inside the temp hash called value, of datatype float OR null" do
-	end
+	# it "should have a key inside the temp hash called value, of datatype float OR nil" do
+	# end
 
-	it "should have a key inside the temp hash called unit, of datatype string" do
-	end
+	# it "should have a key inside the temp hash called unit, of datatype string" do
+	# end
 
-	it "should have a key inside the method hash called twist, of datatype string OR null" do
-	end
+	# it "should have a key inside the method hash called twist, of datatype string OR null" do
+	# end
 
-	it "should have a hash called ingredients" do
-	end
+	# it "should have a hash called ingredients" do
+	# end
 
-	it "should have an array inside the ingredients hash called malt" do
-	end
+	# it "should have an array inside the ingredients hash called malt" do
+	# end
 
-	it "should have a AT LEAST ONE hash inside the malt array" do
-	end
+	# it "should have a AT LEAST ONE hash inside the malt array" do
+	# end
 
-	it "should have a key inside the hash within the malt array called name, of datatype string" do
-	end
+	# it "should have a key inside the hash within the malt array called name, of datatype string" do
+	# end
 
-	it "should have a hash inside the hash within the malt array called amount" do
-	end
+	# it "should have a hash inside the hash within the malt array called amount" do
+	# end
 
-	it "should have a key inside the amount hash called value, of datatype float" do
-	end
+	# it "should have a key inside the amount hash called value, of datatype float" do
+	# end
 
-	it "should have a key inside the amount hash called unit, of datatype string" do
-	end
+	# it "should have a key inside the amount hash called unit, of datatype string" do
+	# end
 
-	it "should have an array inside the ingredients hash called hops" do
-	end
+	# it "should have an array inside the ingredients hash called hops" do
+	# end
 
-	it "should have AT LEAST ONE hash inside the hops array" do
-	end
+	# it "should have AT LEAST ONE hash inside the hops array" do
+	# end
 
-	it "should have a key inside the hash within the hops array called name, of datatype string" do
-	end
+	# it "should have a key inside the hash within the hops array called name, of datatype string" do
+	# end
 
-	it "should have a hash inside the hash within the hops array called amount" do
-	end
+	# it "should have a hash inside the hash within the hops array called amount" do
+	# end
 
-	it "should have a key inside the amount hash called value, of datatype float" do
-	end
+	# it "should have a key inside the amount hash called value, of datatype float" do
+	# end
 
-	it "should have a key inside the amount hash called unit, of datatype string" do
-	end
+	# it "should have a key inside the amount hash called unit, of datatype string" do
+	# end
 
-	it "should have a key inside the hash within the hops array called add, of datatype string" do
-	end
+	# it "should have a key inside the hash within the hops array called add, of datatype string" do
+	# end
 
-	it "should have a key inside the hash within the hops array called attribute, of datatype string" do
-	end
+	# it "should have a key inside the hash within the hops array called attribute, of datatype string" do
+	# end
 
-	it "should have a key inside the ingredients hash called yeast, of datatype string OR null" do
-	end
+	# it "should have a key inside the ingredients hash called yeast, of datatype string OR null" do
+	# end
 
-	it "should have an array called food_pairing" do
-	end
+	# it "should have an array called food_pairing" do
+	# end
 
-	it "should have AT LEAST ONE string inside the food_pairing array" do
-	end
+	# it "should have AT LEAST ONE string inside the food_pairing array" do
+	# end
 
-	it "should have a key called brewers_tips, of datatype string" do
-	end
+	# it "should have a key called brewers_tips, of datatype string" do
+	# end
 
-	it "should have a key called contributed_by, of datatype string" do
-	end
+	# it "should have a key called contributed_by, of datatype string" do
+	# end
 
+	
 
 
 
