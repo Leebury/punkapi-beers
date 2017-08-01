848f736700c4cadb3b34029a0203b3135631e667
Jenny Johnson <kippz07@users.noreply.github.com>
2017-08-01 10:49:47 +0100

    added get_all_beers and get_beer_by_name

--- punk_api.rb
+++ punk_api.rb
@@ -1,3 +1,19 @@
 require 'airborne'
 
 # OUR CLASS/MODULE WILL GO HERE
+
+module PunkApi
+    require 'httparty'
+
+    def self.get_all_beers
+        beers_page_1 = HTTParty.get('https://api.punkapi.com/v2/beers?page=1&per_page=80')
+        beers_page_2 = HTTParty.get('https://api.punkapi.com/v2/beers?page=2&per_page=80')
+        beers_page_3 = HTTParty.get('https://api.punkapi.com/v2/beers?page=3&per_page=80')
+        beers_page_1.push(beers_page_2).push(beers_page_3)
+    end
+
+    def self.get_beer_by_name(name)
+        beer = HTTParty.get("https://api.punkapi.com/v2/beers?beer_name=#{name}")
+    end
+
+end
